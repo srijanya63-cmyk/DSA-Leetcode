1568a1fd7010eb4bc13a2361d9dbfaaf9aa28d5f
Srijanya <srijanya63@gmail.com>
2025-12-24 16:10:24 +0530

    Time: 9 ms (71.91%) | Memory: 48.1 MB (23.08%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -21,7 +21,7 @@ class Solution {
         long hours = 0;
 
         for (int pile : piles) {
-            // 🔥 FORCE LONG ARITHMETIC
+            //  FORCE LONG ARITHMETIC
             hours += ((long)pile + k - 1) / k;
 
             // optimization + overflow safety
