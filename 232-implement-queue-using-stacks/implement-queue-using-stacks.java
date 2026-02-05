a3b581b427c191dba8b75147d18df653f140ee0e
Srijanya <srijanya63@gmail.com>
2026-02-05 23:13:09 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (73.73%) - LeetSync

---
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.java
@@ -0,0 +1,41 @@
+import java.util.Stack;
+
+class MyQueue {
+    Stack<Integer> stack1;
+    Stack<Integer> stack2;
+
+    public MyQueue() {
+        stack1 = new Stack<>();
+        stack2 = new Stack<>();
+    }
+
+    // Push element to back of queue
+    public void push(int x) {
+        stack1.push(x);
+    }
+
+    // Remove front element
+    public int pop() {
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.pop();
+    }
+
+    // Get front element
+    public int peek() {
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.peek();
+    }
+
+    // Check if queue is empty
+    public boolean empty() {
+        return stack1.isEmpty() && stack2.isEmpty();
+    }
+}
