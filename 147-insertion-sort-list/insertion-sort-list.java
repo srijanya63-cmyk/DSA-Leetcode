1c36a1838c683a7992d27f08d944eef70f3f2b82
Srijanya <srijanya63@gmail.com>
2026-03-10 19:04:18 +0530

    Time: 19 ms (39.68%) | Memory: 46.1 MB (93.39%) - LeetSync

---
+++ 147-insertion-sort-list/insertion-sort-list.java
@@ -0,0 +1,27 @@
+class Solution {
+    public ListNode insertionSortList(ListNode head) {
+
+        ListNode dummy = new ListNode(0);
+        ListNode curr = head;
+
+        while(curr != null){
+
+            ListNode prev = dummy;
+
+            // find correct position
+            while(prev.next != null && prev.next.val < curr.val){
+                prev = prev.next;
+            }
+
+            ListNode next = curr.next;
+
+            // insert node
+            curr.next = prev.next;
+            prev.next = curr;
+
+            curr = next;
+        }
+
+        return dummy.next;
+    }
+}
